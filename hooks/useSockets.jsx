5524ac600d3567444279d5b05d5d4ce64586eeb3
Joshua <akinleyejoshua.dev@gmail.com>
2024-11-08 16:21:56 +0100

    Update useSockets.jsx

--- hooks/useSockets.jsx
+++ hooks/useSockets.jsx
@@ -2,12 +2,12 @@ import { useRef } from "react";
 import { io } from "socket.io-client";
 
 const useSocket = () => {
-  const url = "http://localhost:4000/"
-  const liveUrl = "https://ultrasharepro-api.onrender.com"
+  // const url = "http://localhost:4000/"
+  const url = "https://ultrasharepro-api.onrender.com"
 
   let socket = useRef(
     // io("https://ultrasharepro-api.onrender.com"),
-    io(liveUrl, {
+    io(url, {
       autoConnect: false,
     })
   )
